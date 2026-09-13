08f65df808061de596712a4acdc654f6cac6c205
Zheng Junyi <zhengjunyi@live.com>
2026-09-14 00:25:21 +0800

    refactor: simplify build process by removing unnecessary result handling in build.ts

--- build.ts
+++ build.ts
@@ -1,16 +1,7 @@
-const result = await Bun.build({
+await Bun.build({
   entrypoints: ["./src/index.ts", "./src/post.ts"],
   target: "node",
   outdir: "./dist",
   splitting: true,
   format: "esm",
 });
-
-if (!result.success) {
-  for (const log of result.logs) {
-    console.error(log);
-  }
-  process.exit(1);
-}
-
-export {};
